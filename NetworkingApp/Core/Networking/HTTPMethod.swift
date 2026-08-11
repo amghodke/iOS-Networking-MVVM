2a3daf4867dc541fe074fc1a562d2cc3b376b5a6
Amitkumar Ghodke <amitkumar.ghodke@gmail.com>
2026-08-11 13:32:33 +0530

    Add HTTP Method Abstraction

---
+++ NetworkingApp/Core/Networking/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  NetworkingApp
+//
+//  Created by Amitkumar on 11/08/26.
+//
+
+import Foundation
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
