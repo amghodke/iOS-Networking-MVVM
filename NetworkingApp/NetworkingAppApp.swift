b5ec55ba726d9ddb7c56b1a5df75c2e34dd9be29
Amitkumar Ghodke <amitkumar.ghodke@gmail.com>
2026-08-11 13:12:37 +0530

    Initial Commit

---
+++ NetworkingApp/NetworkingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingAppApp.swift
+//  NetworkingApp
+//
+//  Created by Amitkumar on 11/08/26.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
